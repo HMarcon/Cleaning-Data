f74da9578bf186155a3d385316037b2f7afb1055
HMarcon <henriquemmagri42@gmail.com>
2020-06-17 21:44:36 -0300

    Variables names changes for clarity
    Program consolidation and cleaning
    
    Use of melt/dcast to extract measurements by subject/activity

--- run_analysis.R
+++ run_analysis.R
@@ -4,43 +4,44 @@ activity_labels <- as.data.frame(read.table("activity_labels.txt"))
 
 ##Appropriately labels the data set with descriptive variable names.
 
-variables <- as.data.frame(read.table("features.txt"))
+property <- read.table("features.txt")
 
 ##Read data: training and test sets
-train_label <- as.data.frame(read.table("./train/y_train.txt"))
-train_data <- as.data.frame(read.table("./train/X_train.txt"))
-colnames(train_data) <- variables$V2
-train_data$activity <- as.factor(train_label$V1)
+train_label <- read.table("./train/y_train.txt")
+train_data <- read.table("./train/X_train.txt")
+train_subject <- read.table("./train/subject_train.txt")
+train <- cbind(train_data, train_label, train_subject)
+
 
 test_label <- as.data.frame(read.table("./test/y_test.txt"))
 test_data <- as.data.frame(read.table("./test/X_test.txt"))
-colnames(test_data) <- variables$V2
-test_data$activity <- as.factor(test_label$V1)
+test_subject <- as.data.frame(read.table("./test/subject_test.txt"))
+test <- cbind(test_data, test_label, test_subject)
+
 
 ##Merges the training and the test sets to create one data set.
 
-#data_labels <- rbind(as.factor(train_label$V1),as.factor(test_label$V1))
-full_dataset <- rbind(train_data, test_data)
+full_dataset <- rbind(train, test)
+colnames(full_dataset) <- c(property$V2, "activity", "subject" )
+full_dataset$activity <- factor(full_dataset$activity, levels = activity_labels[,1],
+                                labels = activity_labels[,2])
+full_dataset$subject <- as.factor(full_dataset$subject)
 
-##for (i in 1:nrow(full_dataset)){
-##        for (j in 1:nrow(activities)){
-##                if(full_dataset$label[i] == activities[j,1]){
-##                        full_dataset$label[i] <- activities[j,2]
-##                }
-##        }
-##}
+##Extracts only the measurements on the mean and standard deviation for each measurement
+
+Means <- colMeans(full_dataset[1:(ncol(full_dataset)-2)])
+Deviations <- apply(full_dataset[1:(ncol(full_dataset)-2)], 2, sd)
+
 
 ##Extracts only the measurements on the mean and standard deviation for 
-##each measurement.
+##each measurement. 
+full_data_melted <- melt(full_dataset, id = c("subject", "activity"))
 
-Means <- colMeans(full_dataset[-562])
-Deviations <- apply(full_dataset[-562], 2, sd)
+#Average of each property by activity and subject
+full_data_mean <- dcast(full_data_melted, subject + activity ~ variable, mean)
 
-summarized_data <- as.data.frame(rbind(Means, Deviations))
+#Create new file: tidy.txt
+write.table(full_data_mean, "tidy.txt", row.names = FALSE, quote = FALSE)
 
-##From the data set, creates a second, independent tidy data set 
-##with the average of each variable for each activity and each subject.
+#}
 
-##A job for tapply!
-
-indep <- tapply(full_dataset[,1], full_dataset$activity, mean)
